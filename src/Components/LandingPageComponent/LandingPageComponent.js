ab340a98a95b205df6e9bed4604af58e3f1a0756
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-11-22 15:21:25 +0200

    Added react router. Added routes to the error page and home page components

---
+++ src/Components/LandingPageComponent/LandingPageComponent.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class LandingPageComponent extends Component {
+  render() {
+    return (
+      <div>LandingPageComponent</div>
+    )
+  }
+}
