e90606e988f056989a57c501bbac78b98d26692e
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-11-22 16:05:59 +0200

    Added font aweswome location dot icon to the left of the navbar brand

--- src/Components/NavbarComponent/NavbarComponent.js
+++ src/Components/NavbarComponent/NavbarComponent.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react'
 import {Navbar, NavItem, NavLink, Nav, NavbarBrand, Container} from 'react-bootstrap'
+import {} from '@fortawesome/fontawesome-svg-core'
 import 'bootstrap/dist/css/bootstrap.css';
 import './NavbarStyles.css'
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
 export default class NavbarComponent extends Component {
   render() {
     return (
       
       <Navbar>
       <Container>
-        <NavbarBrand href="/">OnSpot</NavbarBrand>
-        <Nav className="mr-auto">
+        <NavbarBrand href="/">
+          <FontAwesomeIcon icon={faLocationDot}/> OnSpot
+        </NavbarBrand>
+        <Nav className="mr-auto"> 
           <Nav.Link href="/ExporeEvents">Explore events</Nav.Link>
           <Nav.Link className= 'hostEventLink' href="/HostEvent">Host an Event</Nav.Link>
 
