835dd18f244b69f80e62acf348e593dcc1ce64c1
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-11-22 21:04:05 +0200

    Added the landing page design

--- src/Components/LandingPageComponent/LandingPageComponent.js
+++ src/Components/LandingPageComponent/LandingPageComponent.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react'
 import NavbarComponentRegisteredUser from '../NavbarComponent/NavbarComponentRegisteredUser'
 import NavbarComponentNotRegisteredUser from '../NavbarComponent/NavbarComponentNotRegisteredUser'
+import { faMapMarkedAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Container } from 'react-bootstrap'
+import conversation from '../../Images/conversation.png'
+import './LandingPageStyles/LandingPage.css'
 
 export default class LandingPageComponent extends Component {
   render() {
     return (
       <div>
         <NavbarComponentNotRegisteredUser/>
+          <Container>
+          <div className='row'>
+            <div className='welcomeText col-sm-6 col-md-6'>
+              <p>Join a meeting you’re excited to attend anywhere, at any time.</p>
+              <div className='signUpButtonWrapper'>
+                <p className = 'signUpButton'>Sign Up</p>
+              </div>
+            </div>
+            <div className='mapIcon col-sm-6 col-md-3'>
+              <FontAwesomeIcon icon={faMapMarkedAlt} fontSize = '320px'/>
+            </div>
+          </div>
+          <div className = 'row secondRow'>
+            <div className = ' welcomeImageWrapper col-sm-6 col-md-6'>
+              <img className = 'welcomeImage' src={conversation}/>
+            </div>
+            <div className='welcomeText col-sm-6 col-md-6'>
+              <p>Your best networking partner; even introverts use it.</p>
+            </div>
+          </div>
+        </Container>
+
       </div>
     )
   }
