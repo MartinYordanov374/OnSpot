ae32d36d0d1c44e7d286c68b653ea32029954559
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-11-23 23:19:32 +0200

    Added styling to the profile page

--- src/Components/ProfilePageComponent/ProfilePageComponent.js
+++ src/Components/ProfilePageComponent/ProfilePageComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Container, Button,Card, } from 'react-bootstrap'
 import NavbarComponentRegisteredUser from '../NavbarComponent/NavbarComponentRegisteredUser'
-
+import './ProfilePageStyles/ProfilePageStyle.css'
 export default class ProfilePageComponent extends Component {
   render() {
     return (
@@ -9,15 +9,17 @@ export default class ProfilePageComponent extends Component {
             <NavbarComponentRegisteredUser/>
             <div className='profilePageWrapper'>
                 <div className='profilePageBackgroundImage'>
-
+                    <img 
+                        src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2F8b%2F16%2F7a%2F8b167af653c2399dd93b952a48740620.jpg&f=1&nofb=1&ipt=33608bf0973b950d8a9032fd47b796c156c60bf3f6edf4b174dc2947f2d9b4da&ipo=images' 
+                        className='userPFP'
+                    />
                 </div>
                 <div className='profilePageUserDetails d-flex'>
-                    <img src='' className='userPFP'></img>
                     <div className='row'>
                         <span className='username col'>Username </span>
                         <span className='followers'>0 followers</span>
                     </div>
-                    <div className='row'>
+                    <div className='row userInteractionBtns'>
                         <span className='messageUser col'>Message</span>
                         <span className='followUser col'>Follow</span>
                     </div>
