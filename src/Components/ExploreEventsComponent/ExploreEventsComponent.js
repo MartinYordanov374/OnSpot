e3da23b8d62260d3ce2bc9f7d767b4920064495d
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-11-23 19:15:50 +0200

    Added styling to the search page

--- src/Components/ExploreEventsComponent/ExploreEventsComponent.js
+++ src/Components/ExploreEventsComponent/ExploreEventsComponent.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react'
+import { Container, FormControl, Button } from 'react-bootstrap'
+import NavbarComponentRegisteredUser from '../NavbarComponent/NavbarComponentRegisteredUser'
+import './ExploreEventsStyles/ExploreEventsStyle.css'
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 export default class ExploreEventsComponent extends Component {
   render() {
     return (
-      <div>ExploreEventsComponent</div>
+      <Container>
+        <NavbarComponentRegisteredUser/>
+        <div className='searchPageWrapper'>
+          <div className='searchBarWrapper d-flex justify-content-center'>
+            <FormControl className='searchBar d-flex' placeholder='Search events'/>
+          </div>
+          <div className='searchResults d-flex justify-content-center'>
+            <h6>Your search results will be shown below</h6>
+          </div>
+        </div>
+
+      </Container>
     )
   }
 }
