eaf345597a18966b9be918468ed2b1c28313fee3
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-11-23 08:58:14 +0200

    Added the registration page skeleton

--- src/Components/RegistrationPageComponent/RegistrationPageComponent.js
+++ src/Components/RegistrationPageComponent/RegistrationPageComponent.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react'
 import NavbarComponentNotRegisteredUser from '../NavbarComponent/NavbarComponentNotRegisteredUser'
-import { Container } from 'react-bootstrap'
+import { Container, InputGroup, Form, Button } from 'react-bootstrap'
 
 export default class RegistrationPageComponent extends Component {
   render() {
     return (
         <Container>
             <NavbarComponentNotRegisteredUser/>
+            <h1>Sign Up</h1>
+            <div className='registrationFormWrapper'>
+                <InputGroup className='mb-5'>
+                    <Form.Control placeholder='Email'/>
+                    <Form.Control placeholder='Preferred username'/>
+                </InputGroup>
+                <InputGroup>
+                    <Form.Control placeholder='Password'/>
+                    <Form.Control placeholder='Confirm password'/>
+                </InputGroup>
+                <div className='buttonWrapper text-center'>
+                    <Button className='mt-3' >Sign Up</Button>
+
+                </div>
+                <div className='AlreadyRegisteredWrapper text-center fixed-bottom'>
+                    <p className='alreadyRegistered'>You already have an account? Log in here.</p>
+                </div>
+            </div>
         </Container>
     )
   }
