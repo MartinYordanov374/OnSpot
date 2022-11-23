86574a2782f2e8af8c5500c850437d1affd1a233
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-11-23 09:49:46 +0200

    Added the login page to the react router

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import LandingPageComponent from './Components/LandingPageComponent/LandingPageComponent';
 import ErrorPage from './Components/ErrorPageComponent/ErorPage';
 import RegistrationPageComponent from './Components/RegistrationPageComponent/RegistrationPageComponent';
+import LoginPageComponent from './Components/LoginPageComponent/LoginPageComponent';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <Routes>
       <Route path = '/' element={<LandingPageComponent/>}/>
       <Route path = '/register' element={<RegistrationPageComponent/>}/>
+      <Route path = '/login' element={<LoginPageComponent/>}/>
 
       <Route path = '*' element={<ErrorPage/>}/>
 
