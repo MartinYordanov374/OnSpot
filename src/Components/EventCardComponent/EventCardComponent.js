dda9c6b4514723f4bba97f37aa54a1b236e1ca88
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-11-23 11:02:41 +0200

    Added styling to the event card component

--- src/Components/EventCardComponent/EventCardComponent.js
+++ src/Components/EventCardComponent/EventCardComponent.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Container,Card } from 'react-bootstrap'
+import '../EventCardComponent/EventCardStyles/EventCard.css'
 export default class EventCardComponent extends Component {
   render() {
     return (
         <Container>
-            <Card>
-                <Card.Header>Event Header</Card.Header>
-                <Card.Body>Event body</Card.Body>
-                <Card.Footer>Event Footer</Card.Footer>
+            <Card className='eventCard'>
+                <Card.Header className = 'eventCardHeader'>Event Header</Card.Header>
+                <Card.Body className = 'eventCardBody'>Event body</Card.Body>
+                <Card.Footer className = 'eventCardFooter'>Event Footer</Card.Footer>
             </Card>
         </Container>
     )
