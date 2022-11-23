ebe6f0d4152ca9caf590e46255fdda291a3d5c92
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-11-23 08:46:49 +0200

    The registration page component is now added to the react router

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import LandingPageComponent from './Components/LandingPageComponent/LandingPageComponent';
 import ErrorPage from './Components/ErrorPageComponent/ErorPage';
+import RegistrationPageComponent from './Components/RegistrationPageComponent/RegistrationPageComponent';
 
 function App() {
   return (
    <Router>
     <Routes>
       <Route path = '/' element={<LandingPageComponent/>}/>
+      <Route path = '/register' element={<RegistrationPageComponent/>}/>
+
       <Route path = '*' element={<ErrorPage/>}/>
 
     </Routes>
