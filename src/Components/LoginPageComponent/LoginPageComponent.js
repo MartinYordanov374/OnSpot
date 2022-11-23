b283c33e5e62f76dd099fcc7531debb1f5c597d3
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-11-23 09:48:11 +0200

    Created the login page component

---
+++ src/Components/LoginPageComponent/LoginPageComponent.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class LoginPageComponent extends Component {
+  render() {
+    return (
+      <div>LoginPageComponent</div>
+    )
+  }
+}
