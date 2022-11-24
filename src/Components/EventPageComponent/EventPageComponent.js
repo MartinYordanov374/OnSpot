5df8cdf63aaeb0ffbdbd86347c39745abf55f36a
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-11-24 18:04:18 +0200

    updated the event card component design

--- src/Components/EventPageComponent/EventPageComponent.js
+++ src/Components/EventPageComponent/EventPageComponent.js
@@ -11,13 +11,15 @@ export default class EventPageComponent extends Component {
                 <Card.Header className = 'eventCardHeader'>
                     <h1>Artificial intelligence outlooks</h1>
                 </Card.Header>
-                <div className='row'>
+                <div className='row eventCardDescWrapper'>
                     <Card.Subtitle className='eventCardDescription col'>
                         Is AI that dangerous technology that people have been warning us about for decades or have we been fooled?
                         <br></br>
-                        <Button className='attendButton'>Attend</Button>
+                        <div className='attendButtonWrapper'>
+                            <Button className='attendButton'>Attend</Button>
+                        </div>
                     </Card.Subtitle>
-                    <div className='col'>
+                    <div className='col eventDetails'>
                         <p className=''>Event Topic: Tech</p>
                         <p className='' >Event type: Public</p>
                     </div>
@@ -25,9 +27,15 @@ export default class EventPageComponent extends Component {
 
                 </div>
                 <div className='row'>
-                    <div className='col'>
-                        <h3>Martin Yordanov</h3>
-                        <p>Martin Yordanov is an AI enthusiast with a humble experience in AI.</p>
+                    <div className='col eventHostData'>
+                        <div className='d-flex eventHostDataContainer'>
+                            <img 
+                            src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2F8b%2F16%2F7a%2F8b167af653c2399dd93b952a48740620.jpg&f=1&nofb=1&ipt=33608bf0973b950d8a9032fd47b796c156c60bf3f6edf4b174dc2947f2d9b4da&ipo=images'
+                            className='eventHostPfp'/>
+                            <h3 className='eventHostName'>Martin Yordanov</h3>
+
+                        </div>
+                        <p className='eventHostBio'>Martin Yordanov is an AI enthusiast with a humble experience in AI.</p>
 
                     </div>
                     <div className='mapWrapper col'>
