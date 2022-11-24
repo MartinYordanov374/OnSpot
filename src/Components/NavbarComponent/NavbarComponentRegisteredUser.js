6ea6619fcc3b506dbb36dac40e1773a3e3ddbbd1
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-11-24 22:24:41 +0200

    Removed the registered user navbar profile image

--- src/Components/NavbarComponent/NavbarComponentRegisteredUser.js
+++ src/Components/NavbarComponent/NavbarComponentRegisteredUser.js
@@ -19,13 +19,6 @@ export default class NavbarComponentRegisteredUser extends Component {
           <Nav.Link href="/ExploreEvents">Explore events</Nav.Link>
           <Nav.Link className= 'hostEventLink' href="/HostEvent">Host an Event</Nav.Link>
 
-          <Nav.Link href="/profile">
-            <img 
-            src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2F8b%2F16%2F7a%2F8b167af653c2399dd93b952a48740620.jpg&f=1&nofb=1&ipt=33608bf0973b950d8a9032fd47b796c156c60bf3f6edf4b174dc2947f2d9b4da&ipo=images'
-            className='ProfilePicture'
-          />
-          </Nav.Link>
-
         </Nav>
       </Container>
     </Navbar>
