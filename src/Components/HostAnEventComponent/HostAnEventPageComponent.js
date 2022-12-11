ad8c5b2db29ce7601a98a9ee1adabcb2794654ec
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-12-11 14:46:06 +0200

    The host an eventg page is now restricted to registered users only

--- src/Components/HostAnEventComponent/HostAnEventPageComponent.js
+++ src/Components/HostAnEventComponent/HostAnEventPageComponent.js
@@ -6,12 +6,13 @@ import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css';
 import SidebarComponent from '../SidebarComponent/SidebarComponent'
 import Axios from 'axios'
+import NonRegisteredLandingPage from '../LandingPageComponent/NonRegisteredLandingPage'
 export default class HostAnEventPageComponent extends Component {
     // TODO: FIGURE LOCATION OUT
     constructor()
     {
         super()
-        this.state = {eventName: '', eventDescription: '', eventType: '', eventCategory: '', eventLocation: '', eventDate: ''}
+        this.state = {eventName: '', eventDescription: '', eventType: '', eventCategory: '', eventLocation: '', eventDate: '', loginStatus: false}
         this.handleSelectType = this.handleSelectType.bind(this)
         this.handleSelectCategory = this.handleSelectCategory.bind(this)
         this.handleSelectLocation = this.handleSelectLocation.bind(this)
@@ -62,76 +63,99 @@ export default class HostAnEventPageComponent extends Component {
             date: this.state.eventDate    
         }, 
         {withCredentials: true})
-        console.log(this.state)
     }
+
+    checkIfUserIsLoggedIn = async () => {
+        await Axios.get('http://localhost:3030/isUserLoggedIn', {withCredentials: true})
+        .then((res)=>{
+          if(res.data == true)
+          {
+            
+              this.setState({'loginStatus': true})
+          }
+          else
+          {
+            this.setState({'loginStatus': false})
+          }})
+      }
+
+      componentDidMount()
+      {
+        this.checkIfUserIsLoggedIn()
+      }
   render() {
     return (
         <div>
+            {this.state.loginStatus == true ? 
+                <div>
+                    <SidebarComponent/>
+                    <Container>
+                        <NavbarComponentRegisteredUser/>
+                        <Card className='HostEventCard'>
+                            <Card.Body>
+                                <div className='row'>
+                                    <div className='eventNameWrapper col-sm mt-5'>
+                                        <h2 className='fieldLabel'>Event name</h2>
+                                        <FormControl className='inputField' placeholder='Enter your event Name' onChange = {(e) => this.handleEnterEventName(e.target.value)}/>
+                                    </div>
+                                    <div className='eventTypeWrapper col-sm mt-5'>
+                                        <h2 className='fieldLabel'>Event type</h2>
+                                        <InputGroup>
+                                        <FormControl className='inputField eventTypeField' placeholder='Public' aria-describedby='dropdownAddon' disabled='true'/>
+                                            <DropdownButton className='inputFieldDropdown' id='dropdownAddon' onSelect={this.handleSelectType}>
+                                                <Dropdown.Item eventKey = {'Public'}>Public</Dropdown.Item>
+                                                <Dropdown.Item eventKey = {'Private'} >Private</Dropdown.Item>
+                                            </DropdownButton>
+                                        
+                                        </InputGroup>
+                                    </div>
+                                </div>
+                                <div className='row'>
+                                    <div className='eventDescriptionWrapper col-sm mt-5'>
+                                        <h2 className='fieldLabel'>Event description</h2>
+                                        <FormControl className='inputField' placeholder='Event description' onChange = {(e) => this.handleEnterEventDescription(e.target.value)}/>
+                                    </div>
+                                    <div className='eventCategoryWrapper col-sm mt-5'>
+                                        <h2 className='fieldLabel'>Event category</h2>
+                                        <InputGroup>
+                                            <FormControl className='inputField eventCategoryField' placeholder='e.g. Tech' disabled='true'/>
+                                            <DropdownButton className='inputFieldDropdown' id='dropdownAddon' onSelect={this.handleSelectCategory}>
+                                                <Dropdown.Item eventKey = {'Tech'}>Tech</Dropdown.Item>
+                                                <Dropdown.Item eventKey = {'Business'} >Business</Dropdown.Item>
+                                                <Dropdown.Item eventKey = {'Hangout'} >Hangout</Dropdown.Item>
+                                                <Dropdown.Item eventKey = {'Other'} >Other</Dropdown.Item>
+                                            </DropdownButton>
+                                        </InputGroup>
+                                    </div>
+                                </div>
 
-            <SidebarComponent/>
-            <Container>
-                <NavbarComponentRegisteredUser/>
-                <Card className='HostEventCard'>
-                    <Card.Body>
-                        <div className='row'>
-                            <div className='eventNameWrapper col-sm mt-5'>
-                                <h2 className='fieldLabel'>Event name</h2>
-                                <FormControl className='inputField' placeholder='Enter your event Name' onChange = {(e) => this.handleEnterEventName(e.target.value)}/>
-                            </div>
-                            <div className='eventTypeWrapper col-sm mt-5'>
-                                <h2 className='fieldLabel'>Event type</h2>
-                                <InputGroup>
-                                <FormControl className='inputField eventTypeField' placeholder='Public' aria-describedby='dropdownAddon' disabled='true'/>
-                                    <DropdownButton className='inputFieldDropdown' id='dropdownAddon' onSelect={this.handleSelectType}>
-                                        <Dropdown.Item eventKey = {'Public'}>Public</Dropdown.Item>
-                                        <Dropdown.Item eventKey = {'Private'} >Private</Dropdown.Item>
-                                    </DropdownButton>
+                                <div className='row'>
+                                <div className='eventCategoryWrapper col-sm mt-5'>
+                                        <h2 className='fieldLabel'>Event Location</h2>
+                                        <FormControl className='inputField eventLocationField' placeholder='e.g. Menlo Park'/>
+
+                                    </div>
+                                    <div className='eventCategoryWrapper col-sm mt-5'>
+                                        <h2 className='fieldLabel'>Event Date</h2>
+                                        <InputGroup>
+                                            <FormControl className='inputField eventDateField' placeholder='e.g. 28/01/2022' disabled='true'/>
+                                            <DropdownButton className='inputFieldDropdown' id='dropdownAddon' drop='start'>
+                                                <Calendar onChange={this.handleSelectDate}/>
+                                            </DropdownButton>
+                                        </InputGroup>
+                                    </div>
+                                </div>
                                 
-                                </InputGroup>
-                            </div>
-                        </div>
-                        <div className='row'>
-                            <div className='eventDescriptionWrapper col-sm mt-5'>
-                                <h2 className='fieldLabel'>Event description</h2>
-                                <FormControl className='inputField' placeholder='Event description' onChange = {(e) => this.handleEnterEventDescription(e.target.value)}/>
-                            </div>
-                            <div className='eventCategoryWrapper col-sm mt-5'>
-                                <h2 className='fieldLabel'>Event category</h2>
-                                <InputGroup>
-                                    <FormControl className='inputField eventCategoryField' placeholder='e.g. Tech' disabled='true'/>
-                                    <DropdownButton className='inputFieldDropdown' id='dropdownAddon' onSelect={this.handleSelectCategory}>
-                                        <Dropdown.Item eventKey = {'Tech'}>Tech</Dropdown.Item>
-                                        <Dropdown.Item eventKey = {'Business'} >Business</Dropdown.Item>
-                                        <Dropdown.Item eventKey = {'Hangout'} >Hangout</Dropdown.Item>
-                                        <Dropdown.Item eventKey = {'Other'} >Other</Dropdown.Item>
-                                    </DropdownButton>
-                                </InputGroup>
-                            </div>
-                        </div>
-
-                        <div className='row'>
-                        <div className='eventCategoryWrapper col-sm mt-5'>
-                                <h2 className='fieldLabel'>Event Location</h2>
-                                <FormControl className='inputField eventLocationField' placeholder='e.g. Menlo Park'/>
-
-                            </div>
-                            <div className='eventCategoryWrapper col-sm mt-5'>
-                                <h2 className='fieldLabel'>Event Date</h2>
-                                <InputGroup>
-                                    <FormControl className='inputField eventDateField' placeholder='e.g. 28/01/2022' disabled='true'/>
-                                    <DropdownButton className='inputFieldDropdown' id='dropdownAddon' drop='start'>
-                                        <Calendar onChange={this.handleSelectDate}/>
-                                    </DropdownButton>
-                                </InputGroup>
-                            </div>
-                        </div>
-                        
-                        <div className='buttonWrapper d-flex justify-content-center mt-3'>
-                            <Button className='hostEventButton' onClick={() => this.HostEvent()}>Host event</Button>
-                        </div>
-                    </Card.Body>
-                </Card>
-            </Container>
+                                <div className='buttonWrapper d-flex justify-content-center mt-3'>
+                                    <Button className='hostEventButton' onClick={() => this.HostEvent()}>Host event</Button>
+                                </div>
+                            </Card.Body>
+                        </Card>
+                    </Container>
+                </div>
+            : 
+                <NonRegisteredLandingPage/>
+            }
     
         </div>)
   }
