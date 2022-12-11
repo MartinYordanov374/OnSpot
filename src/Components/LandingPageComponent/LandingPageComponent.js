42aa8e51e3bd021cc1539ab99029efa0eb74411c
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-12-11 14:36:43 +0200

    The llanding page is now split into two landing pages - one for the registered and non-registered user.

--- src/Components/LandingPageComponent/LandingPageComponent.js
+++ src/Components/LandingPageComponent/LandingPageComponent.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react'
-import NavbarComponentRegisteredUser from '../NavbarComponent/NavbarComponentRegisteredUser'
-import NavbarComponentNotRegisteredUser from '../NavbarComponent/NavbarComponentNotRegisteredUser'
-import { faMapMarkedAlt } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Container } from 'react-bootstrap'
-import conversation from '../../Images/conversation.png'
 import './LandingPageStyles/LandingPage.css'
-import EventCardComponent from '../EventCardComponent/EventCardComponent';
-import SidebarComponent from '../SidebarComponent/SidebarComponent';
 import Axios from 'axios'
+import NonRegisteredLandingPage from './NonRegisteredLandingPage';
+import RegisteredLandingPage from './RegisteredLandingPage'
 
 export default class LandingPageComponent extends Component {
   constructor()
   {
     super()
-    this.state = {loginStatus: false, events: []}
+    this.state = {loginStatus: false}
   }
+
   checkIfUserIsLoggedIn = async () => {
     await Axios.get('http://localhost:3030/isUserLoggedIn', {withCredentials: true})
     .then((res)=>{
@@ -29,58 +24,23 @@ export default class LandingPageComponent extends Component {
         this.setState({'loginStatus': false})
       }})
   }
+  
+
   componentDidMount()
   {
-    this.getEvents()
     this.checkIfUserIsLoggedIn()
   }
-  getEvents = async () => {
-    let events = await Axios.get('http://localhost:3030/getAllEvents', {withCredentials: true})
-    .then((res) => {this.setState({'events': res.data.payload})})
-    .catch((err) => {console.log(err)})
-  }
   render() {
-    
+    // TODO: ADD CUSTOM LOADING PAGE UNTIL PAGE IS READY
     return (
       <div>
         {this.state.loginStatus == false ?
         <div>
-          <NavbarComponentNotRegisteredUser/>
-          <Container>
-          <div className='row'>
-            <div className='welcomeText col-sm-6 col-md-6'>
-              <p>Join a meeting you’re excited to attend anywhere, at any time.</p>
-              <div className='signUpButtonWrapper'>
-                <a href='/register' className='signUpButtonLink'> 
-                  <p className = 'signUpButton'>Sign Up</p>
-                </a>
-              </div>
-            </div>
-            <div className='mapIcon col-sm-6 col-md-3'>
-              <FontAwesomeIcon icon={faMapMarkedAlt} fontSize = '320px'/>
-            </div>
-          </div>
-          <div className = 'row secondRow'>
-            <div className = ' welcomeImageWrapper col-sm-6 col-md-6'>
-              <img className = 'welcomeImage' src={conversation}/>
-            </div>
-            <div className='welcomeText col-sm-6 col-md-6'>
-              <p>Your best networking partner; even introverts use it.</p>
-            </div>
-          </div>
-          </Container>
+            <NonRegisteredLandingPage/>
         </div>
         :
         <div>
-          <SidebarComponent/>
-          <Container>
-            <NavbarComponentRegisteredUser/>
-            {this.state.events.map((event) => {
-              return <EventCardComponent props={event}/>
-
-            })}
-            
-          </Container>
+            <RegisteredLandingPage/>
         </div>
         }
       </div>
---
+++ src/Components/LandingPageComponent/RegisteredLandingPage.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import EventCardComponent from '../EventCardComponent/EventCardComponent';
+import SidebarComponent from '../SidebarComponent/SidebarComponent';
+import NavbarComponentRegisteredUser from '../NavbarComponent/NavbarComponentRegisteredUser'
+import { Container } from 'react-bootstrap'
+import Axios from 'axios'
+
+export default class RegisteredLandingPage extends Component {
+    constructor()
+    {
+      super()
+      this.state = {events: []}
+    }
+
+    componentDidMount()
+    {
+      this.getEvents()
+    }
+    getEvents = async () => {
+      let events = await Axios.get('http://localhost:3030/getAllEvents', {withCredentials: true})
+      .then((res) => {this.setState({'events': res.data.payload})})
+      .catch((err) => {console.log(err)})
+    }
+  render() {
+    return (
+        <div>
+            <SidebarComponent/>
+            <Container>
+                <NavbarComponentRegisteredUser/>
+                {this.state.events.map((event) => {
+                    return <EventCardComponent props={event}/>
+                })}
+            </Container>
+        </div>
+    )
+  }
+}
