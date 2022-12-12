3c00dff88d5e84251b7125727c56acc2cd1cf802
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-12-12 23:34:02 +0200

    Users can now change the year on their calendar schedule.

--- src/Components/CalendarScheduleComponent/CalendarSchedule.js
+++ src/Components/CalendarScheduleComponent/CalendarSchedule.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import MonthComponent from './MonthComponent'
 import MonthSelector from './MonthSelector'
+import YearSelector from './YearSelector'
 
 export default class CalendarSchedule extends Component {
   render() {
     return (
       <div className='CalendarWrapper'>
         <div className='CalendarContainer mx-auto'>
+            <YearSelector yearData = {{currentYear: new Date().getUTCFullYear()}}/>
             <MonthSelector monthData = {{currentMonth: new Date().getMonth()}}/>
             <MonthComponent/>
         </div>
