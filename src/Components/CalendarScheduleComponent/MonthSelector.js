126393d5de987c41d868834eeb705a8400f0e545
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-12-12 23:30:52 +0200

    Users can now change the calendar schedule months backward and forward

--- src/Components/CalendarScheduleComponent/MonthSelector.js
+++ src/Components/CalendarScheduleComponent/MonthSelector.js
@@ -7,17 +7,35 @@ export default class MonthSelector extends Component {
     {
         super()
         this.state = {currentMonth: 0}
-        this.months = ['Jan', 'Feb', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        this.months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
     }
     componentDidMount(){
-        this.setState({'currentMonth' : this.months[this.props.monthData.currentMonth - 1]})
+        this.setState({'currentMonth' : this.props.monthData.currentMonth})
+    }
+    goMonthBack = () => {
+        let previousMonth = this.state.currentMonth - 1
+        console.log(previousMonth)
+        if(previousMonth < 0)
+        {
+            previousMonth = 11
+        }
+        this.setState({'currentMonth' : previousMonth })
+    }
+    goMonthForward = () => {
+        let NextMonth = this.state.currentMonth + 1
+        console.log(NextMonth)
+        if(NextMonth > 11)
+        {
+            NextMonth = 0
+        }
+        this.setState({'currentMonth' : NextMonth })
     }
   render() {
       return (
           <div className='MonthSelectorWrapper'>
-            <FontAwesomeIcon icon = {faCaretLeft} className = 'CaretLeft caret'/>
-            <input type='text' className='form-control MonthSelector' disabled placeholder={this.state.currentMonth}/>
-            <FontAwesomeIcon icon = {faCaretRight} className = 'CaretLeft caret'/>
+            <FontAwesomeIcon icon = {faCaretLeft} className = 'CaretLeft caret' onClick={() => this.goMonthBack()}/>
+            <input type='text' className='form-control MonthSelector' disabled placeholder={this.months[this.state.currentMonth]}/>
+            <FontAwesomeIcon icon = {faCaretRight} className = 'CaretLeft caret' onClick={() => this.goMonthForward()}/>
         </div>
     )
     
