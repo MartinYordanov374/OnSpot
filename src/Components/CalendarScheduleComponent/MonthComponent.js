3ec4dee86de5292ba337b4b3dfdde33307c30c2a
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-12-12 22:31:11 +0200

    The weekday names are now shown above each weekday

--- src/Components/CalendarScheduleComponent/MonthComponent.js
+++ src/Components/CalendarScheduleComponent/MonthComponent.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import WeekComponent from './WeekComponent'
+import WeekdaysComponent from './WeekdaysComponent'
 
 export default class MonthComponent extends Component {
   render() {
     return (
       <div className='MonthComponentWrapper'>
+        <WeekdaysComponent/>
         <div className='MonthComponentContainerFirstWeek week row'>
           <WeekComponent weekData = {{'daysAmount': 7, 'startingDay': 1}}/>
         </div>
