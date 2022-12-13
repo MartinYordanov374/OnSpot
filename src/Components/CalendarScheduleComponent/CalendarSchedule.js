f13cf23915e8bbf022128632de27c488407abe89
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-12-13 19:11:24 +0200

    centered the calendar.. sort of

--- src/Components/CalendarScheduleComponent/CalendarSchedule.js
+++ src/Components/CalendarScheduleComponent/CalendarSchedule.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react'
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from "moment";
+import NavbarComponentRegisteredUser from '../NavbarComponent/NavbarComponentRegisteredUser'
+import SidebarComponent from '../SidebarComponent/SidebarComponent';
+import { Container } from 'react-bootstrap'
+
+import './Styles/CalendarScheduleStyle.css'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
+
 export default class CalendarSchedule extends Component {
   constructor()
   {
@@ -22,19 +28,23 @@ export default class CalendarSchedule extends Component {
   }
   render() {
     return (
-      <div className='CalendarWrapper'>
-        <Calendar className='c1'
-          localizer={this.localizer}
-          defaultView = 'month'
-          views={['month', 'week', 'day']}
-          events={this.state.events}
-          startAccessor="start"
-          endAccessor="end"
-          style={{ height: 500, backgroundColor: 'white' }}
-          eventPropGetter={() => ({
-            style: { backgroundColor: "#72B2E4", fontWeight: 'bold' }
-          })}
-        />
+      <div className='CalendarWrapper justify-content-center align-items-center'>
+        <SidebarComponent/> 
+        <Container className='CalendarContainer'>
+          <NavbarComponentRegisteredUser/>
+            <Calendar
+              localizer={this.localizer}
+              defaultView = 'month'
+              views={['month', 'week', 'day']}
+              events={this.state.events}
+              startAccessor="start"
+              endAccessor="end"
+              style={{ height: 800, backgroundColor: 'white' }}
+              eventPropGetter={() => ({
+                style: { backgroundColor: "#72B2E4", fontWeight: 'bold' }
+              })}/>
+
+        </Container>
       </div>
     )
   }
