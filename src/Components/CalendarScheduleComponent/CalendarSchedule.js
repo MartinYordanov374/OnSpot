59f5815d77e338127183d6dfc89dc40f932e5d78
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-12-14 15:03:12 +0200

    The user events are now fetched from the backend and stotred in the react state of the calendar component

--- src/Components/CalendarScheduleComponent/CalendarSchedule.js
+++ src/Components/CalendarScheduleComponent/CalendarSchedule.js
@@ -4,6 +4,7 @@ import moment from "moment";
 import NavbarComponentRegisteredUser from '../NavbarComponent/NavbarComponentRegisteredUser'
 import SidebarComponent from '../SidebarComponent/SidebarComponent';
 import { Container } from 'react-bootstrap'
+import Axios from 'axios'
 
 import './Styles/CalendarScheduleStyle.css'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
@@ -14,37 +15,73 @@ export default class CalendarSchedule extends Component {
     super()    
     this.localizer = momentLocalizer(moment)
     this.state = 
-      { events: [{
-        start: moment().toDate(),
-        end: moment()
-          .add(1, "days")
-          .toDate(),
-        title: "Покланяне на слънцето"
-      },
-    ]
-      }
-  
-  
+      { upcomingEvents: [],
+        attendedEvents: [],
+        hostedEvents: [],
+        currentUserData: [],
+        isLoading: true
+    }
+  }
+  componentDidMount = async() => {
+    await Axios.get('http://localhost:3030/getUserData', {withCredentials: true})
+    .then(async (res) => {
+      this.setState({'currentUserData': res.data[0]})
+      this.getUpcomingEvents()
+      this.getAttendedEvents()
+      this.getHostedEvents()
+      this.setState({'isLoading': false})
+    })
+  }
+
+  getCurrentUserData = async() => {
+    let res = await Axios.get('http://localhost:3030/getUserData', {withCredentials: true})
+
+    await this.getUpcomingEvents()
+
+  }
+  getUpcomingEvents = async() => {
+    let res = await Axios.get(`http://localhost:3030/GetAllUpcomingUserEvents/${this.state.currentUserData.id}`, {withCredentials: true})
+    this.setState({'upcomingEvents': res.data})
+    await this.getAttendedEvents()
+  }
+  getAttendedEvents = async() => {
+    await Axios.get(`http://localhost:3030/GetAllAttendedUserEvents/${this.state.currentUserData.id}`, {withCredentials: true})
+    .then(async (res) => {
+      this.setState({'attendedEvents': res.data})
+      await this.getHostedEvents()
+
+    })
+  }
+  getHostedEvents = async() => {
+    await Axios.get(`http://localhost:3030/GetAllEventsHostedByUser/${this.state.currentUserData.id}`, {withCredentials: true})
+    .then(async (res) => {
+      this.setState({'hostedEvents': res.data})
+    })
+
   }
   render() {
     return (
       <div className='CalendarWrapper justify-content-center align-items-center'>
         <SidebarComponent/> 
-        <Container className='CalendarContainer'>
-          <NavbarComponentRegisteredUser/>
-            <Calendar
-              localizer={this.localizer}
-              defaultView = 'month'
-              views={['month', 'week', 'day']}
-              events={this.state.events}
-              startAccessor="start"
-              endAccessor="end"
-              style={{ height: 800, backgroundColor: 'white' }}
-              eventPropGetter={() => ({
-                style: { backgroundColor: "#72B2E4", fontWeight: 'bold' }
-              })}/>
+        {this.state.isLoading == true ? 
+            "Loading"
+         : 
+         <Container className='CalendarContainer'>
+         <NavbarComponentRegisteredUser/>
+           <Calendar
+             localizer={this.localizer}
+             defaultView = 'month'
+             views={['month', 'week', 'day']}
+             events={this.state.hostedEvents}
+             startAccessor="start"
+             endAccessor="end"
+             style={{ height: 800, backgroundColor: 'white' }}
+             eventPropGetter={() => ({
+               style: { backgroundColor: "#72B2E4", fontWeight: 'bold' }
+             })}/>
 
-        </Container>
+       </Container>}
+
       </div>
     )
   }
