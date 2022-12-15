6f4ee3001e19c92dceee6f05960a20063d2a3258
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-12-15 20:02:27 +0200

    Added styling to the chat box

--- src/Components/ChatboxModalComponent/ChatBoxModalComponent.js
+++ src/Components/ChatboxModalComponent/ChatBoxModalComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Modal, Button } from 'react-bootstrap'
+import { Modal, Button, FormControl } from 'react-bootstrap'
 import './Styles/ChatboxModal.css'
 export default class ChatBoxModalComponent extends Component {
     
@@ -9,14 +9,30 @@ export default class ChatBoxModalComponent extends Component {
       <div  className={this.props.props.isModalShown == true ? " modal modal-visible" : " modal modal-hidden"}>
         <Modal.Dialog>
         <Modal.Header closeButton onClick={() => this.props.props.modalHandler()}>
-          <Modal.Title>Chatbox title</Modal.Title>
+        <img 
+                // src={
+                //   `data: image/png;base64,
+                //   ${Buffer.from(this.state.userData.ProfilePicture.data).toString('base64')}`
+                //   }
+                src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2F8b%2F16%2F7a%2F8b167af653c2399dd93b952a48740620.jpg&f=1&nofb=1&ipt=33608bf0973b950d8a9032fd47b796c156c60bf3f6edf4b174dc2947f2d9b4da&ipo=images'
+                className='userPFP'
+        />
+          <Modal.Title>Username</Modal.Title>
         </Modal.Header>
 
         <Modal.Body>
-          <p>Chatbox body</p>
+          <div className='chatWrapper'>
+            <div className='receiverMessage message'>Hello?</div>
+            <div className='senderMessage message'>Hello, is this Seeker?</div>
+            <div className='receiverMessage message'>Yes, it is me!</div>
+            <div className='senderMessage message'>GOSH! THE EVENT LAST WEEK WAS ABSOLUTELY AMAZING!!!</div>
+            <div className='receiverMessage message'>I know right! I'm about to host another one soon! Stay tuned !</div>
+          </div>
         </Modal.Body>
 
         <Modal.Footer>
+          <FormControl className='sendMessageInputField'>
+            </FormControl>
           <Button variant="primary">Send</Button>
         </Modal.Footer>
       </Modal.Dialog>
