ee2f4101ed36fdb6c43ec8452986f1e802c1ea20
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-12-18 12:48:02 +0200

    Clicking on an event card now leads to the said event

--- src/Components/SquareEventCardComponent/SquareEventCardComponent.js
+++ src/Components/SquareEventCardComponent/SquareEventCardComponent.js
@@ -10,43 +10,45 @@ export default class SquareEventCardComponent extends Component {
   render() {
     let eventData = this.props;
     return (
-    //   <div className='EventCardWrapper'>
-        <Card className='EventCard col-lg-5 col-md-8 col-sm-8'>
+        // ADD LINK TO EVENTS
+        <Card className='EventCard col-lg-5 col-md-8 col-sm-8' >
 
             <Card.Body className='EventCardBody'>
-                <Card.Img
-                    className='EventCardImage'
-                    src={`https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fedgewood.org%2Fwp-content%2Fuploads%2F2020%2F08%2FiStock-1172741523-scaled.jpg&f=1&nofb=1&ipt=a4878aef5df609e9ad98d20d4a6bc68b145813865386519dbcb42199c651669c&ipo=images`}
-                />
-                <Card.Title className='EventCardTitle'>
-                    {eventData.props.EventName}
-                </Card.Title>
-                <Card.Subtitle className='EventCardDate'>
-                    Jan 22 2023 - Jan 23 2023
-                </Card.Subtitle>
-                <div className='EventDetailsContainer row'>
-                    <Card.Text className='EventClass col-lg-4'>
-                        {eventData.props.EventClass == 'Business' ? <FontAwesomeIcon icon={faChartLine}/> : ""}
-                        {eventData.props.EventClass == 'Tech' ? <FontAwesomeIcon icon={faMicrochip}/> : ""}
-                        {eventData.props.EventClass == 'Hangout' ? <FontAwesomeIcon icon={faComment}/> : ""}
-                        {eventData.props.EventClass == 'Other' ? <FontAwesomeIcon icon={faBullhorn}/> : ""}
+                <Card.Link href={`/Event/${eventData.props.EventID}`}>
+                    <Card.Img
+                        className='EventCardImage'
+                        src={`https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fedgewood.org%2Fwp-content%2Fuploads%2F2020%2F08%2FiStock-1172741523-scaled.jpg&f=1&nofb=1&ipt=a4878aef5df609e9ad98d20d4a6bc68b145813865386519dbcb42199c651669c&ipo=images`}
+                    />
+                    <Card.Title className='EventCardTitle'>
+                        {eventData.props.EventName}
+                    </Card.Title>
+                    <Card.Subtitle className='EventCardDate'>
+                        Jan 22 2023 - Jan 23 2023
+                    </Card.Subtitle>
+                    <div className='EventDetailsContainer row'>
+                        <Card.Text className='EventClass col-lg-4'>
+                            {eventData.props.EventClass == 'Business' ? <FontAwesomeIcon icon={faChartLine}/> : ""}
+                            {eventData.props.EventClass == 'Tech' ? <FontAwesomeIcon icon={faMicrochip}/> : ""}
+                            {eventData.props.EventClass == 'Hangout' ? <FontAwesomeIcon icon={faComment}/> : ""}
+                            {eventData.props.EventClass == 'Other' ? <FontAwesomeIcon icon={faBullhorn}/> : ""}
 
-                        {eventData.props.EventClass}
-                    </Card.Text>
-                    <Card.Text className='EventType col-lg-5'>
-                        {eventData.props.EventType == 1 ? <FontAwesomeIcon icon={faLock}/> : <FontAwesomeIcon icon={faLockOpen}/>}
-                        {eventData.props.EventType == 1 ? 'Private' : 'Public'}
-                    </Card.Text>
-                </div>
-                <div className='EventInteractionButtons'>
-                    <Button className='InterestedButton Button'> 
-                        <FontAwesomeIcon icon={faStar}/> Interested
-                    </Button>
-                    <Button className='GoingButton Button'> 
-                        <FontAwesomeIcon icon={faPersonWalkingArrowRight}/> Going
-                    </Button>
+                            {eventData.props.EventClass}
+                        </Card.Text>
+                        <Card.Text className='EventType col-lg-5'>
+                            {eventData.props.EventType == 1 ? <FontAwesomeIcon icon={faLock}/> : <FontAwesomeIcon icon={faLockOpen}/>}
+                            {eventData.props.EventType == 1 ? 'Private' : 'Public'}
+                        </Card.Text>
+                    </div>
+                </Card.Link>
+                    <div className='EventInteractionButtons'>
+                        <Button className='InterestedButton Button'> 
+                            <FontAwesomeIcon icon={faStar}/> Interested
+                        </Button>
+                        <Button className='GoingButton Button'> 
+                            <FontAwesomeIcon icon={faPersonWalkingArrowRight}/> Going
+                        </Button>
 
-                </div>
+                    </div>
 
             </Card.Body>
         </Card>
