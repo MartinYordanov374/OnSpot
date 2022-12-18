6cd4c265f9c55e9438020e3957a5dca7b0579306
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-12-18 18:12:51 +0200

    Implemented levenshtein distance algorithm for the search functionality.

---
+++ Backend/KeyWordSimilarityAlgorithm/LevenshteinDistance.js
@@ -0,0 +1,30 @@
+function findDistance(userWord, keyword)
+{
+    const matrix = []
+    for(let i = 0; i < userWord.length + 1; i++)
+    {
+        const row = []
+        for(let j = 0; j < keyword.length + 1; j++)
+        {
+            row.push(j)
+        }
+        row[0] = i
+        matrix.push(row)
+    }
+
+    for(let i = 1; i < userWord.length + 1; i++)
+    {
+        for(let j = 1; j < keyword.length + 1; j++)
+        {
+            if(userWord[i - 1] == keyword[j - 1])
+            {
+                matrix[i][j] = matrix[i-1][j-1]
+            }
+            else
+            {
+                matrix[i][j] = 1 + Math.min(matrix[i-1][j-1], matrix[i][j-1], matrix[i-1][j])
+            }
+        }
+    }
+    return matrix[userWord.length - 1][keyword.length - 1]
+}
