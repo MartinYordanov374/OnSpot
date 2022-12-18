76767c9969ca89757d20d04efa58c2a4b9a5c34e
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-12-18 12:16:26 +0200

    The landing page event cards now have a shadow.

--- src/Components/SquareEventCardComponent/SquareEventCardComponent.js
+++ src/Components/SquareEventCardComponent/SquareEventCardComponent.js
@@ -11,7 +11,7 @@ export default class SquareEventCardComponent extends Component {
     let eventData = this.props;
     return (
     //   <div className='EventCardWrapper'>
-        <Card className='EventCard col-lg-5 col-md-9 col-sm-9'>
+        <Card className='EventCard col-lg-5 col-md-9 col-sm-9' >
 
             <Card.Body className='EventCardBody'>
                 <Card.Img
@@ -41,24 +41,6 @@ export default class SquareEventCardComponent extends Component {
                 {/* <Card.Text  className='EventCardDescription'>
                     {eventData.props.EventDescription.length > 17 ? eventData.props.EventDescription.slice(0, 17) + '...' : eventData.props.EventDescription}
                 </Card.Text> */}
-                <div className='EventHosterDataContainer'>
-                    {eventData.props.ProfilePicture.data
-                      ?
-                      <Card.Img 
-                            src={
-                              `data: image/png;base64,
-                              ${Buffer.from(eventData.props.ProfilePicture.data).toString('base64')}`
-                              }
-                            className='eventHostPFP'
-                        />
-                      :
-                      <Card.Img 
-                          src={`https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2F8b%2F16%2F7a%2F8b167af653c2399dd93b952a48740620.jpg&f=1&nofb=1&ipt=33608bf0973b950d8a9032fd47b796c156c60bf3f6edf4b174dc2947f2d9b4da&ipo=images`}
-                          className='eventHostPFP'
-                        />
-                    }   
-                    <Card.Text className='EventHosterUsername'>{eventData.props.Username}</Card.Text>
-                </div>
                 <div className='EventInteractionButtons'>
                     <Button className='LearnMoreButton'>Learn More</Button>
                 </div>
