0abbb992adf4849a28b8fe9219bc4dbd4eac29a9
GoofyGalaxy374 <prophetyordanov@gmail.com>
2023-01-07 14:18:26 +0200

    Updated the get all events query, since I no longer need the profile picture of the hoster with the event data.

--- Backend/Server/Services/EventsService/EventsService.js
+++ Backend/Server/Services/EventsService/EventsService.js
@@ -104,13 +104,12 @@ async function GetAllEvents()
 {
     try{
         // let result = await sql.query`SELECT * FROM dbo.Events`
+        //TODO: FIX THE BUG WHICH CAUSES
         let result = await sql.query`
-        SELECT EventName, EventDescription, EventType, EventClass, EventID, EventLocation, Username, id as UserID, ProfilePicture
+        SELECT EventName, EventDescription, EventType, EventClass, EventID, EventLocation, Username, id as UserID
         FROM Events e
         JOIN Users u
-        ON e.EventHosterID  = u.id 
-        JOIN ProfilePictures pp 
-        ON u.id = pp.UserID`
+        ON e.EventHosterID  = u.id `
         return {status: 200, msg: 'Events successfully fetched from the database.', payload: result.recordset}
     }
     catch(err)
