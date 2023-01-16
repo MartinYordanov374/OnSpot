7c0eb8d1dc966921ec264296ef465ae21d62f262
GoofyGalaxy374 <martinyordanov284@gmail.com>
2023-01-16 13:40:29 +0200

    Initialized the settings component

---
+++ src/Components/SettingsComponent/Settings.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import './Styles/SettingsStyles.css'
+export default class Settings extends Component {
+  render() {
+    return (
+      <div>Settings</div>
+    )
+  }
+}
