fdb7814a2c5b333aeee68df039df912a36aa8eb1
GoofyGalaxy374 <prophetyordanov@gmail.com>
2023-01-21 22:32:27 +0200

    Post owner data (username and profile picture) are now shown on the posts page.

--- src/Components/PostComponent/PostComponent.js
+++ src/Components/PostComponent/PostComponent.js
@@ -4,6 +4,8 @@ import { faComment, faCommentAlt, faShare, faShareAlt, faShareAltSquare, faShare
 import { Container, Button,Card, } from 'react-bootstrap'
 import Axios from 'axios'
 import './Styles/PostStyles.css'
+import { Buffer } from 'buffer';
+
 export default class PostComponent extends Component {
 
   constructor()
@@ -17,45 +19,72 @@ export default class PostComponent extends Component {
   getPosterData = async(userID) => {
     await Axios.post(`http://localhost:3030/getUserDataById/${userID}`, {}, {withCredentials: true})
     .then((res) => {
-      console.log(res)
-      this.setState({'postOwnerData': res.data})
+      this.setState({'postOwnerData': res.data}, () => {
+        this.setState({'isLoading': false})
+
+      })
     })
   }
 
   componentDidMount = () => {
     this.getPosterData(this.props.postData.UserID)
+
   }
   render() {
     return (
-    <Card className='eventPost'>
-        <Card.Header>
-
-          {this.state.postOwnerData
-          ? 
-            <span className='posterUsername'> {this.state.postOwnerData.Username}</span>
-          :
-            ""
-          }
-        </Card.Header>
-        <Card.Body>
-          <Card.Text>
-            {this.props.postData.PostContent}
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer className='postInteractionButtons'>
-          <div className='row'>
-            <span className='col-sm-4 interactionButton'>
-              <FontAwesomeIcon icon={faThumbsUp}/> Like
-            </span>
-            <span className='col-sm-4 interactionButton'>
-              <FontAwesomeIcon icon={faCommentAlt}/> Comment
-            </span>
-            <span className='col-sm-4 interactionButton'>
-              <FontAwesomeIcon icon={faShare}/> Share
-            </span>
+      <div>
+        {this.state.isLoading == false ?
+          <Card className='eventPost'>
+            <Card.Header>
+            {this.state.postOwnerData.ProfilePicture.data 
+                        ?
+                          <img 
+                              src={
+                                `data: image/png;base64,
+                                ${Buffer.from(this.state.postOwnerData.ProfilePicture.data).toString('base64')}`
+                                }
+                              className='posterPFP'
+                          />
+                        :
+                          <img 
+                            src={`${this.state.postOwnerData.ProfilePicture}`}
+                            className='posterPFP'
+                          />
+              }
+              {this.state.postOwnerData
+              ? 
+                <span className='posterUsername'> {this.state.postOwnerData.Username}</span>
+              :
+                ""
+              }
+            </Card.Header>
+            <Card.Body>
+              <Card.Text>
+                {this.props.postData.PostContent}
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer className='postInteractionButtons'>
+              <div className='row'>
+                <span className='col-sm-4 interactionButton'>
+                  <FontAwesomeIcon icon={faThumbsUp}/> Like
+                </span>
+                <span className='col-sm-4 interactionButton'>
+                  <FontAwesomeIcon icon={faCommentAlt}/> Comment
+                </span>
+                <span className='col-sm-4 interactionButton'>
+                  <FontAwesomeIcon icon={faShare}/> Share
+                </span>
+              </div>
+            </Card.Footer>
+          </Card>
+        :
+          <div className='d-flex justify-content-center'>
+            <div class="spinner-border text-primary loadingSpinnerWrapper" role="status">
+              <span class="visually-hidden">Loading...</span>
+            </div>
           </div>
-        </Card.Footer>
-      </Card>
+        }
+      </div>
     )
   }
 }
